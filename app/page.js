f64a11fb0d32c7be4d1abc8f87a3f01da67d988d
unijajang <zifnffk32123@gmail.com>
2025-02-14 23:35:57 +0900

    DB 세팅

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
 
 // 'use client'
 
-export default function Home() {
+import { connectDB } from "@/util/database";
+import { MongoClient } from "mongodb";
+
+
+
+export default async function Home() {
+
+  const client = await connectDB;
+  const db = client.db("forum")
+  let result = await db.collection('post').find().toArray()
+  // post 컬렉션에 있는 모든 데이터를 가져와서 Array 로 변환
+
+  console.log(result);
+
+
   return (
     <div><h1>안녕하세요</h1></div>
   );
